73a3858bb3325820a7987580bb181ce33447252f
Richard Fagundes Rodrigues <richardsiacc@Richards-MacBook-Pro.local>
2025-08-08 14:23:43 -0300

    Starting project

---
+++ MyMani/MyMani/MyManiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyManiApp.swift
+//  MyMani
+//
+//  Created by Richard Fagundes Rodrigues on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MyManiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
